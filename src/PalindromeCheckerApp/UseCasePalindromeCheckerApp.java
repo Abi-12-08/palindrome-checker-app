7907791f9a94b0357a6ce7f22c524d4622774982
Joanna Jacob <joannajacob910@gmail.com>
2026-02-19 09:44:48 +0530

    Base application setup

---
+++ src/PalindromeCheckerApp/UseCasePalindromeCheckerApp.java
@@ -0,0 +1,13 @@
+package PalindromeCheckerApp;
+import java.util.Scanner;
+public class UseCasePalindromeCheckerApp {
+    public static void main(String[] args){
+        System.out.println("Welcome to the Palindrome Checker App Management System!! ");
+
+    }
+
+
+
+
+
+}
